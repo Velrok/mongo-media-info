b59444df01fcb91f39711760864fe5a100cf47de
Waldemar Schwan <waldemar.schwan@adcloud.com>
2013-04-20 12:44:44 +0200

    init

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Distutils',
+      version='0.0',
+      description='mongo-media-info',
+      author='Velrok',
+      author_email='waldemar.schwan@gmail.com',
+      url='https://github.com/Velrok/mongo-media-info',
+      packages=['distutils', 'distutils.command', 'mongo_media_info'],
+     )
